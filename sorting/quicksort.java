b79a4f3148520fe4abf0c79c06b2279680e423cf
Samiksha gupta <samikshagupta0299@gmail.com>
2026-07-25 07:28:14 +0530

    search

--- sorting/quicksort.java
+++ sorting/quicksort.java
@@ -38,9 +38,43 @@ public class quicksort {
         }
         System.out.println();
     }
+    // search in sorted rotated sort array
+    public static int search(int arr[],int tar, int si, int ei){
+        if(si>ei){
+            return -1;
+        }
+        int mid = si+(ei-si)/2;
+        if(arr[mid]==tar){
+            return mid;
+        }
+        //mid on l1
+        if(arr[si] <=arr[mid]){
+            if(arr[si] <= tar && tar <= arr[mid]){
+                return search(arr, tar, si, mid-1);
+            }else{
+                return search(arr, tar, mid+1, ei);
+            }
+        }
+        else{
+            if (arr[mid] <= tar && tar <=arr[ei]){
+                return search(arr, tar, mid+1, ei);
+
+            }else{
+                return search(arr, tar, si, mid-1);
+            }
+        }
+    }
+
+
+
     public static void main(String args[]) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
-        quicksorting(arr, 0,arr.length-1);
-        printArr(arr);
+        int arr[] = {4, 5, 6, 7, 0, 1,8};
+        // quicksorting(arr, 0,arr.length-1);
+        // printArr(arr);
+        int target = 0;
+        int tarIdx = search(arr, target, 0, arr.length-1);
+        System.out.println(tarIdx);
+
     }
 }
+
