944ee7a761e922e0b97d0fcf0887f159257e3c98
Samiksha gupta <samikshagupta0299@gmail.com>
2026-07-30 23:11:44 +0530

    bubble-sort

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -1,5 +1,26 @@
 package sorting;
 
 public class bubblesort {
-    
+    public static void bubblessort(int arr[],int n){
+        for(int i =0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if (arr[j]> arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void PrintArray(int arr[],int n){
+        for(int i=0;i<n;i++){
+            System.err.print(arr[i]+" ");
+        };
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int arr[] ={4,3,1,5,2};
+        bubblessort(arr, n);
+        PrintArray(arr,n);
+    }
 }
