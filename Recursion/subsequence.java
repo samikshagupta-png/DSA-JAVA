5b034e994106f28d165382436b03a42a244b1acb
Samiksha gupta <samikshagupta0299@gmail.com>
2026-08-31 07:46:32 +0530

    unique subsequence code

--- Recursion/subsequence.java
+++ Recursion/subsequence.java
@@ -1,21 +1,30 @@
 package Recursion;
 
+import java.util.HashSet;
+//unique subsequence
 public class subsequence {
-    public static void stringsubsequence(String str , int idx,String newString){
+    public static void stringsubsequence(String str , int idx,String newString, HashSet<String>set ){
         if(idx == str.length()){
-            System.out.println(newString);
-            return;
+            if(set.contains(newString)){
+                return;
+            }else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+                    
+            }
         }
         
         char currChar = str.charAt(idx);
         // to be 
-        stringsubsequence(str, idx+1,newString+currChar);
+        stringsubsequence(str, idx+1,newString+currChar,set);
         // not to be 
-        stringsubsequence(str, idx+1, newString);
+        stringsubsequence(str, idx+1, newString,set);
     }
     public static void main(String[] args) {
         String str ="abc";
-        stringsubsequence(str, 0, "");
+        HashSet<String> set = new HashSet<>();
+        stringsubsequence(str, 0, "",set);
         
     }
     
