21d1e6acad5da9772ab9b5e334631e1c7f0bef12
Samiksha gupta <samikshagupta0299@gmail.com>
2026-09-11 09:08:49 +0530

    intoduction to arraylist

---
+++ Arraylist/operation.java
@@ -0,0 +1,20 @@
+package Arraylist;
+import java.util.ArrayList;
+
+// import Arrayist.*;
+public class operation {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+        list1.add(5);
+        list2.add("str");
+        list3.add(true);
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println(list3);
+        
+        
+    }
+    
+}
